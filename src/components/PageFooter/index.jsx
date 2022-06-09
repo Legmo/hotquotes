8129dcdb5d271ad478dee5a878d08455084ab430
Vladimir Degtiarev <mail@legmo.ru>
2022-06-10 01:09:39 +0800

    #20: Create basic page layout (#46)

--- src/components/PageFooter/index.jsx
+++ src/components/PageFooter/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from './style.module.css';
-import Navigation from '../Navigation';
+// import Navigation from '../Navigation';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
@@ -9,7 +9,8 @@ const iconMail = <FontAwesomeIcon icon={faEnvelope} size="1x" />;
 const Footer = () => {
   return (
     <footer className={style.footer}>
-      <Navigation />
+      {/*<Navigation />*/}
+      <div></div>
       <nav className="font-weight-light small text-right mx-auto pt-1">
         <a
           href="&#109;&#97;i&#108;&#116;o&#58;mail&#64;&#108;%&#54;5%67&#37;6Do&#46;%&#55;2&#117;"
