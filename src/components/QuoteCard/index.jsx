6eae8600ce1eb2bbfb6caef0fe3fc7704369e61c
Vladimir Degtiarev <mail@legmo.ru>
2022-06-15 11:35:12 +0800

    Issue 55 (#56)
    * Develop (#54)
    
    * IDE CodeStyles fixes
    
    * Minor index.html fixes
    
    * #20: Create basic page layout (#46)
    
    * Issue 20 (#50)
    
    * #20: Create basic page layout
    
    * #20: Create basic page layout. SCSS & HTML fixes. Install Lodash
    
    * #20: Create basic page layout. SCSS & HTML fixes. Install Lodash
    
    * Issue 20 (#53)
    
    * #20: Create basic page layout
    
    * #20: Create basic page layout. SCSS & HTML fixes. Install Lodash
    
    * #20: Create basic page layout. SCSS & HTML fixes. Install Lodash
    
    * #20: Create basic page layout. SCSS & HTML fixes
    
    * Merge conflict
    
    * #20: Create basic page layout. SCSS & HTML fixes
    
    * Minor index.html fixes
    
    * Resolve conflict
    
    * Issue 20 (#53)
    
    * #20: Create basic page layout
    
    * #20: Create basic page layout. SCSS & HTML fixes. Install Lodash
    
    * #20: Create basic page layout. SCSS & HTML fixes. Install Lodash
    
    * #20: Create basic page layout. SCSS & HTML fixes
    
    * Merge conflict
    
    * #20: Create basic page layout. SCSS & HTML fixes
    
    * Basic html layout fixes

--- src/components/QuoteCard/index.jsx
+++ src/components/QuoteCard/index.jsx
@@ -28,14 +28,16 @@ class QuoteCard extends Component {
       <figure className={style.blockquoteContainer}>
         <blockquote className={style.blockquote}>{quote.text}</blockquote>
         <figcaption className={style.blockquoteFooter}>
-          <span className={style.author}>{quote.author[0].name}</span>{' '}
-          <cite className={style.source}>
-            {quote.title !== '' ? (
-              <span>, «{quote.title}»</span>
-            ) : (
-              <span></span>
-            )}
-          </cite>
+          <span className={style.authorAndSource}>
+            <span className={style.author}>{quote.author[0].name}</span>{' '}
+            <cite className={style.source}>
+              {quote.title !== '' ? (
+                <span>, «{quote.title}»</span>
+              ) : (
+                <span></span>
+              )}
+            </cite>
+          </span>
           <ListTagsInline listItems={quote.category} />
         </figcaption>
       </figure>
